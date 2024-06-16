e78837a2ce1b187945d9551c73439ab6531c1dc0
JLehnen33022 <jameslehnen310@gmail.com>
2024-06-16 17:08:07 -0400

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,4 +1,4 @@
-// James Lehnen, CEN-3024C, 5-26-24
+// James Lehnen, CEN-3024C, 5-26-24; UPDATED 6-16-24
 // Software Development I
 // Book Class collects the information of the text file - the ID, Title and Author.
 
@@ -6,30 +6,25 @@ public class Book {
     private int id;
     private String title;
     private String author;
-
+    private boolean ifStocked;
+    
     // Constructor
     public Book(int id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
+        this.ifStocked = true;
     }
 
-    // Getters
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
+    // Get/Set
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public boolean isStocked() { return ifStocked; }
+    public void setStock(boolean available) { ifStocked = available; }
 
     // toString method to represent the book as a string
     @Override
     public String toString() {
-        return id + ". " + title + "," + author;
+        return id + ", " + title + ", " + author + ", " + (ifStocked ? "In Stock" : "Withdrawn");
     }
 }
